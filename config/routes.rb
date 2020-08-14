950f8a3ed913f39d82197ef40ddb09f29668359b
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-08-15 03:57:27 +0900

    非ログイン時のバグ修正

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,4 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   resources :relationships, only: [:create, :destroy]
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
