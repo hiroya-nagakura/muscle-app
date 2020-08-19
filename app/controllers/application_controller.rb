db74ff596654294fdd13c862d44a6340430e1e88
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-08-19 15:00:28 +0900

    トップページの表示改善

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  def set_search
+    @search = Article.ransack(params[:q])
+    @search_articles = @search.result(distinct: true)
+    @search_ary = Article.all.select(:target_site).distinct
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  
   def home
+    @articles = Article.all
   end
 end
