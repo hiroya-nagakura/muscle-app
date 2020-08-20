3275849190e8cc1ff5f140ab0aa3e20eb2514f35
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-08-20 21:13:25 +0900

    トップページに人気メニュー新着メニュー表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   
   def home
-    @articles = Article.all
+    @all_ranks = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(10).pluck(:article_id))
+    @new_menu = Article.all.order(updated_at: 'DESC').limit(10)
   end
 end
