051be95c253fafabe30441d864ee3ca5de076cbb
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-08-28 12:02:51 +0900

    request/commentのrspec作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,31 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_target_article
 
   def create
     @comment = current_user.comments.new(comment_params)
     if @comment.save
-      redirect_back(fallback_location: root_path)
+      redirect_to article_path(@article), notice: 'コメントを投稿しました。'
     else
-      redirect_back(fallback_location: root_path)
+      flash.now[:alert] = '投稿に失敗しました。'
+      redirect_to article_path(@article) 
     end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     if @comment.destroy
-      redirect_back(fallback_location: root_path)
+      redirect_to article_path(@article) 
     end
   end
 
   private
   def comment_params
     params.require(:comment).permit(:content, :article_id)
+  end
+
+  def set_target_article
+    @article = Article.find(params[:article_id])
   end
 
 end
---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe "Comments", type: :request do
+
+  let(:user) { create(:user) }
+  let(:other) { create(:user) }
+  before do
+    @article =  create(:article)
+  end
+
+  describe "#create" do
+    context '未ログイン状態のとき' do
+      it 'コメントを作成できず、ログインページにリダイレクトされること' do
+        comment_params = attributes_for(:comment, article_id: @article.id)
+        expect do
+          post article_comments_path(@article), params: {comment: comment_params }
+        end.to change(Comment.all, :count).by(0)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to '/users/sign_in'
+      end
+
+    end
+    context 'ログイン状態のとき' do
+      it 'コメントを作成でき、コメントした記事にリダイレクトされる' do
+        sign_in(user)
+        comment_params = attributes_for(:comment, article_id: @article.id)
+        expect do
+          post article_comments_path(@article), params: {comment: comment_params }
+        end.to change(Comment.all, :count).by(1)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to article_path(@article)
+      end
+    end
+  end
+
+  describe "#destroy" do
+    context '未ログイン状態のとき' do
+      it 'コメントを削除できず、ログインページにリダイレクトされること' do
+        comment = create(:comment, article_id: @article.id)
+        expect do
+          delete article_comment_path(article_id: @article.id, id: comment.id)
+        end.to change(Comment.all, :count).by(0)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+    context 'ログイン状態のとき' do
+      it 'コメントを削除でき、コメントしていた記事にリダイレクトされること' do
+        sign_in(user)
+        comment = create(:comment,user: user, article_id: @article.id)
+        expect do
+          delete article_comment_path(article_id: @article.id, id: comment.id)
+        end.to change(Comment.all, :count).by(-1)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to redirect_to article_path(@article)
+      end
+    end
+  end
+end
