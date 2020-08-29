6e4363361811309ee9686ac236184e713773b81f
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-08-29 17:31:33 +0900

    request/favoriteのrspec作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,18 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_article
 
   def create
-    @article = Article.find(params[:article_id])
     @favorite = current_user.favorites.create(article_id: params[:article_id])
-    @favorite_count = Favorite.where(article_id: params[:article_id]).count
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
     @favorite = Favorite.find_by(user_id: current_user.id, article_id: params[:article_id])
     @favorite.destroy
-    @favorite_count = Favorite.where(article_id: params[:article_id]).count
   end
 
+  private
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
 end
---
+++ spec/requests/favorites_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "Favorites", type: :request do
+  let (:user) {create(:user) }
+
+  before do
+    @article = create(:article)
+  end
+
+  describe "#create" do
+    context '未ログイン状態のとき' do
+      it 'いいねできないこと' do
+        expect do
+          post article_favorites_path(@article.id),xhr: true
+        end.to change(Favorite.all, :count).by(0)
+      end
+    end
+    context 'ログイン状態のとき' do
+      it 'いいねできること' do
+        sign_in(user)
+        expect do
+          post article_favorites_path(@article.id),xhr: true
+        end.to change(Favorite.all, :count).by(1)
+      end
+    end
+  end
+
+  describe '#destroy' do
+    before do
+      @favorite = create(:favorite, user: user, article: @article)
+    end
+    context '未ログイン状態のとき' do
+      it 'いいね解除できないこと' do
+        expect do
+          delete article_favorite_path(article_id: @article.id, id: @article.favorites[0].id),xhr: true
+        end.to change(Favorite.all, :count).by(0)
+      end
+    end
+    context 'ログイン状態のとき' do
+      it 'いいねを解除できること' do
+        sign_in(user)
+        expect do
+          delete article_favorite_path(article_id: @article.id, id: @article.favorites[0].id),xhr: true
+        end.to change(Favorite.all, :count).by(-1)
+      end
+    end
+  end
+end
