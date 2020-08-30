f94bb37ec874ac333afcb1120a7b289d03c4f74d
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-08-30 13:09:35 +0900

    comment/featureのrspec作成

---
+++ spec/features/comments_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.feature "Comments", type: :feature do
+  before do
+    user = create(:user,
+                  user_name: 'tester',
+                  email: 'test@example.com',
+                  password: 'password'
+                  )
+    @article = create(:article, user: user)
+  end
+  scenario 'コメントの作成、編集、削除をする' do
+    #トップページを開く
+    visit root_path
+
+    #ログインページへ
+    click_link 'login'
+
+    #ログインする
+    fill_in "メールアドレス", with: 'test@example.com'
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
+    expect(page).to have_text('ログインしました')
+
+    #投稿一覧ページへ
+    click_link 'メニュー一覧'
+
+    #投稿詳細ページへ
+    click_link 'もっと読む'
+
+    #コメント作成
+    expect do
+      fill_in 'comment_content', with:'コメント投稿のテスト'
+      click_button 'コメントする'
+    end.to change(Comment.all, :count).by(1)
+
+    #コメントが反映されたか検証
+    comment = Comment.first
+    expect(comment.content).to eq 'コメント投稿のテスト'
+    expect(current_path).to eq article_path(@article)
+    expect(page).to have_text('コメント投稿のテスト')
+    #ーーーーコメントの編集（未実装）ーーーーー
+
+    #ーーーーコメントの削除ーーーー
+    expect do
+      click_link 'コメントを削除'
+    end.to change(Comment.all, :count).by(-1)
+  end
+end
