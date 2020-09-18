3dbe25dfe9af26a494c5cf6f77ae4b94d939cd64
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-09-19 01:00:15 +0900

    S3への保存を一時保留　#10

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,11 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -39,9 +35,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
+  # def extension_whitelist
+  #   %w(jpg jpeg gif png)
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
