6121790e8087e63964fc9576ca5e35c0d00c93a3
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-09-19 13:49:18 +0900

    トップの記事表示レスポンシブ化 #1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require('jquery')
 
 //追加
 import 'javascripts/scroll-button'
+import 'javascripts/slide'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
