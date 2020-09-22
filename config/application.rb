eec939856a75c797665d7f043711f9a57116aa5b
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-09-23 03:27:48 +0900

    シンプルカレンダーのCSS調整

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module MuscleApp
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
+    config.beginning_of_week = :sunday
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
