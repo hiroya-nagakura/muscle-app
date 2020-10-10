5f2bc449d4a1bcae05eb364d98d616bc222ddbc9
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-10-10 12:12:14 +0900

    画像をリサイズして保存するよう設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_fit: [800, 800]
   #
   # def scale(width, height)
   #   # do something
@@ -35,9 +35,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
