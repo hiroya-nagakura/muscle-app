86cc51cf2d94475e25016abd7733f17bb1809463
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-10-10 21:03:21 +0900

    一覧表示改善

--- app/javascript/javascripts/slide.js
+++ app/javascript/javascripts/slide.js
@@ -1,9 +1,18 @@
 if (window.matchMedia('(max-width: 768px)').matches) {
   $(document).on('turbolinks:load', function() {
     $('.slide').slick({
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      slidesToShow: 2,
+      responsive: [
+        {
+          breakpoint: 450,
+          settings: {
+            slidesToShow: 1
+          }
+        },
+      ]
     });
   });
-}
+} 
