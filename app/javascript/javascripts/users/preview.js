3210331a950ab1e60593119147143da217f68503
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-10-23 03:48:09 +0900

    プロフィール画像のプレビュー機能追加

---
+++ app/javascript/javascripts/users/preview.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function() {
+  var file_image = document.getElementById('prof-file')
+  file_image.addEventListener('change', function (e) {
+    var file = e.target.files[0];
+    var fileReader = new FileReader();
+    fileReader.onload = function() {
+      var img = document.getElementById('img-preview');
+      img.src = this.result;
+    };
+    fileReader.readAsDataURL(file);
+  });
+})
