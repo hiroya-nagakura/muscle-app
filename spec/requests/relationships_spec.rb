a08005b45bd2068d2d4d0a3a7a88d55bda49b5da
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-11-08 23:27:43 +0900

    Update relationships_spec.rb

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'Relationships', type: :request do
       end
     end
     context 'ログイン状態のとき' do
-      it 'フォローすることができること' do
+      it 'フォローできること' do
         sign_in(@user)
         expect do
           post relationships_path, params: { follow_id: @follow.id }, xhr: true
@@ -29,14 +29,14 @@ RSpec.describe 'Relationships', type: :request do
       @relationship = create(:relationship, user: @user, follow: @follow)
     end
     context '未ログイン状態のとき' do
-      it 'フォロー解除することができないこと' do
+      it 'フォロー解除できないこと' do
         expect do
           delete relationship_path(@relationship), params: { follow_id: @follow.id }, xhr: true
         end.to change(Relationship.all, :count).by(0)
       end
     end
     context 'ログイン状態のとき' do
-      it 'フォローを解除できるこ' do
+      it 'フォローを解除できること' do
         sign_in(@user)
         expect do
           delete relationship_path(@relationship), params: { follow_id: @follow.id }, xhr: true
