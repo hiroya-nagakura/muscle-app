d6c0faeb1c6973f4df8029862f66a9a4327f8428
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-11-13 18:56:10 +0900

    Update scroll-button.js

--- app/javascript/javascripts/static_pages/scroll-button.js
+++ app/javascript/javascripts/static_pages/scroll-button.js
@@ -1,9 +1,8 @@
 document.addEventListener('turbolinks:load', function() {
-	var target_favorite = document.getElementById('favorite-menu-card');
+	const target_favorite = document.getElementById('favorite-menu-card');
 	if (target_favorite){
 		// 移動幅を取得 //
-		var scrollvalue_favorite = target_favorite.clientWidth;
-	}
+		const scrollvalue_favorite = target_favorite.clientWidth;
 	// クリックしたら右、もしくは左へ移動幅分動く
 	$('#favorite-scroll-left').on('click', function() {
 		$(target_favorite).animate({scrollLeft: target_favorite.scrollLeft - scrollvalue_favorite}, 500, "swing");
@@ -12,11 +11,13 @@ document.addEventListener('turbolinks:load', function() {
 	$('#favorite-scroll-right').on('click', function() {
 		$(target_favorite).animate({scrollLeft: target_favorite.scrollLeft + scrollvalue_favorite}, 500, "swing");
 	});
-
-	var target_new = document.getElementById('new-menu-card');
-	if (target_new){
-		var scrollvalue_new = target_new.clientWidth;
 	}
+
+	const target_new = document.getElementById('new-menu-card');
+	if (target_new){
+		// 移動幅を取得 //
+		const scrollvalue_new = target_new.clientWidth;
+	// クリックしたら右、もしくは左へ移動幅分動く
 	$('#new-scroll-left').on('click', function() {
 		$(target_new).animate({scrollLeft: target_new.scrollLeft - scrollvalue_new}, 500, "swing");
 	});
@@ -24,4 +25,5 @@ document.addEventListener('turbolinks:load', function() {
 	$('#new-scroll-right').on('click', function() {
 		$(target_new).animate({scrollLeft: target_new.scrollLeft + scrollvalue_new}, 500, "swing");
 	});
+	}
 });
